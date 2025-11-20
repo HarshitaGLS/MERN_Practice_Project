38208f808ea859b55083fe9e50bb08a0dde7f76f
harshita <harshita.maheshwari@glsuniversity.ac.in>
2025-11-20 08:44:24 +0530

    final

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
+import mongoose from 'mongoose'
 import indexRoute from './routes/indexRoute.js'
 
 const server = express()
